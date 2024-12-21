ed6be8c0a0d214134246605dc621f1ba9c42cf96
Dev Dragon <71600486+dev-Dragon-ahmed@users.noreply.github.com>
2024-12-20 19:11:11 -0500

    start

---
+++ bank/src/app/app/account/components/account-detail/account-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-detail',
+  imports: [],
+  templateUrl: './account-detail.component.html',
+  styleUrl: './account-detail.component.scss'
+})
+export class AccountDetailComponent {
+
+}
