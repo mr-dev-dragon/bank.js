9ed170f63e95fc9d2c4682b865bf2cbbf0a77aa6
Dev Dragon <71600486+dev-Dragon-ahmed@users.noreply.github.com>
2024-12-22 20:58:20 -0500

    add

--- bank/src/app/auth/forgot-password/forgot-password.component.ts
+++ bank/src/app/auth/forgot-password/forgot-password.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'forgot-password',
   imports: [],
+  standalone: true,
+
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
